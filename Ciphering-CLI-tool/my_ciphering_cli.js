e6aded49d3926c71099c24e2ed14468f6d096355
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-11-21 16:48:28 +0200

    fix: fix my_ciphering_cli

--- Ciphering-CLI-tool/my_ciphering_cli.js
+++ Ciphering-CLI-tool/my_ciphering_cli.js
@@ -6,33 +6,33 @@ const { createOutputStream } = require('./src/output-stream');
 const { CipherTransform } = require('./src/transform-caesar');
 const { SEPARATOR_CONFIG } = require('./src/constants');
 
-try {
-  const args = process.argv.slice(2);
-  const setting = parseInputLine(args);
-  let inputStream = createInputputStream(setting);
-  const outputStream = createOutputStream(setting);
-  const config = setting.config.split(SEPARATOR_CONFIG);
+  try {
+    const args = process.argv.slice(2);
+    const setting = parseInputLine(args);
+    let inputStream = createInputputStream(setting);
+    const outputStream = createOutputStream(setting);
+    const config = setting.config.split(SEPARATOR_CONFIG);
 
-  let mainstrean;
-  for (let i = 0; i < config.length; i++) {
-    if (i === 0) {
-      mainstrean = inputStream.pipe(new CipherTransform(config[i]));
+    let mainstrean;
+    for (let i = 0; i < config.length; i++) {
+      if (i === 0) {
+        mainstrean = inputStream.pipe(new CipherTransform(config[i]));
+      } else {
+        mainstrean = mainstrean.pipe(new CipherTransform(config[i]));
+      }
+    }
+
+    mainstrean.pipe(outputStream);
+    process.on('exit', () => process.stdout.write('Thank you. Application is close. Good luck!'));
+    process.on('SIGINT', () => {
+      process.exit();
+    });
+
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      console.error(err.message);
+      process.exit(err.code);
     } else {
-      mainstrean = mainstrean.pipe(new CipherTransform(config[i]));
+      throw err;
     }
   }
-
-  mainstrean.pipe(outputStream);
-  process.on('exit', () => process.stdout.write('Thank you. Application is close. Good luck!'));
-  process.on('SIGINT', () => {
-    process.exit();
-  });
-
-} catch (err) {
-  if (err instanceof ValidationError) {
-    console.error(err.message);
-    process.exit(err.code);
-  } else {
-    throw err;
-  }
-}
